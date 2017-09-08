246f579402b94bdd8ec8eea76b416e0bf8acb67c
winton <ywd979@foxmail.com>
2017-09-08 15:12:58 +0800

    befor mockito

--- code/nymph-Themis/src/test/java/com/themis/test/user/UserInfoTest.java
+++ code/nymph-Themis/src/test/java/com/themis/test/user/UserInfoTest.java
@@ -1,22 +1,46 @@
 package com.themis.test.user;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.themis.base.BaseTest;
+import com.themis.user.controller.UserInfoController;
 import com.themis.userinfo.entity.UserInfo;
 import com.themis.userinfo.service.UserInfoService;
 
 public class UserInfoTest extends BaseTest{
 	
+	@InjectMocks
+	UserInfoController controller;
+	
+	private MockMvc mockMvc;
+	
 	@Autowired
 	UserInfoService service;
+	
+	@Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
 	
 	@Test
 	public void testSelectByPK() {
 		UserInfo userInfo = service.selectByPrimaryKey(1);
 		Assert.assertEquals("admin", userInfo.getNickname());
+	}
+	
+	@Test
+	public void testController() {
+		controller.test(1);
 	}
 	
 }
