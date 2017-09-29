3548b001246c9e7fea7757daaeefb392e6fb30f7
winton <ywd979@foxmail.com>
2017-09-29 12:55:05 +0800

    添加实体类builder pattern

--- code/nymph-core/src/main/java/com/themis/userinfo/entity/UserInfoExtra.java
+++ code/nymph-core/src/main/java/com/themis/userinfo/entity/UserInfoExtra.java
@@ -1,5 +1,35 @@
 package com.themis.userinfo.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class UserInfoExtra extends UserInfo {
 
+	private static final long serialVersionUID = -4448387767744983827L;
+	
+	private String hi;
+
+	@Builder
+	public UserInfoExtra(Integer id, String nickname, String suid, String token, String signInName,
+			String signInPassword, String salt, Integer createTime, Integer enabled, String headUrl, Integer userType,
+			Integer updateTime, String hi) {
+		super(id, nickname, suid, token, signInName, signInPassword, salt, createTime, enabled, headUrl, userType,
+				updateTime);
+		this.hi = hi;
+	}
+
+	public static class UserInfoExtraBuilder extends UserInfoBuilder {
+		UserInfoExtraBuilder() {
+			super();
+		}
+	}
+	
+	public static void main(String[] args) {
+		UserInfoExtra build = new UserInfoExtra.UserInfoExtraBuilder().nickname("132").hi("123").build();
+		System.out.println(build);
+	}
+	
 }
